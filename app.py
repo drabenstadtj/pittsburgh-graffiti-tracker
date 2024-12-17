97a4608e04eb4bf5d7be23fb215f475fb1668055
Hleb Ksianevich <108957681+gleb-ks@users.noreply.github.com>
2024-12-17 14:39:32 -0500

    flask app

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/add_graffiti')
+def add_graffiti():
+    return render_template('add_graffiti.html')
+
 if __name__ == "__main__":
     app.run()
